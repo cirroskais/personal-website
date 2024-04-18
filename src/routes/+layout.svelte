f48684b0141ecf53957a87af36e9d5999840f28d
cirroskais <cirroskais@madhouselabs.net>
2024-04-18 08:52:22 -0400

    opengraph test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,6 +16,15 @@
 		href="https://cdn.discordapp.com/avatars/{$page.data.discord.id}/{$page.data.discord
 			.avatar}.png"
 	/>
+	<meta property="og:title" content={$page.data.discord.username} />
+	<meta property="og:url" content={$page.url.toString()} />
+	<meta
+		property="og:image"
+		content="https://cdn.discordapp.com/avatars/{$page.data.discord.id}/{$page.data.discord
+			.avatar}.png"
+	/>
+	<meta property="og:description" content="Full stack JavaScript developer." />
+	<meta name="theme-color" content="#ffffff" />
 </svelte:head>
 
 <div class="w-screen h-[3rem]">
