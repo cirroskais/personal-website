b648ada5ba3bf774e2af4818180fd3dce0030852
cirroskais <cirroskais@madhouselabs.net>
2024-04-22 04:56:20 -0400

    projects page placeholder

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,3 +5,9 @@
 <svelte:head>
 	<title>{$page.data.discord?.username}/projects</title>
 </svelte:head>
+
+<div class="flex w-full h-[calc(100vh-5.125rem)]">
+	<p class="m-auto text-2xl italic text-neutral-700">
+		There doesn't appear to be anything here yet...
+	</p>
+</div>
