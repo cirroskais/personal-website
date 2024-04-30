a1931b8fe92fa559ac9608717e720cedc5dc9085
cirroskais <cirroskais@madhouselabs.net>
2024-04-30 17:17:00 -0400

    vest png

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -79,7 +79,7 @@
 				<ImgButton href="https://twitter.com/Porpss1" src="/img/buttons/rovin.png"></ImgButton>
 				<ImgButton href="https://split.pet" src="/img/buttons/split.png"></ImgButton>
 				<ImgButton href="https://m1cro.xyz/" src="/img/buttons/micro.png"></ImgButton>
-				<ImgButton href="https://vea.st/" src="/img/buttons/veast.webp"></ImgButton>
+				<ImgButton href="https://vea.st/" src="/img/buttons/veast.png"></ImgButton>
 				<ImgButton href="https://liloandstit.ch/" src="/img/buttons/stitch.png"></ImgButton>
 				<ImgButton href="https://www.mozilla.org/en-US/firefox/new/" src="/img/buttons/firefox.gif"
 				></ImgButton>
