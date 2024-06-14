32c2b9b5eeb453bf7923c4527b0004bad01f9ec9
cirroskais <cirroskais@madhouselabs.net>
2024-06-14 01:45:39 -0400

    kill cache

--- src/lib/server/discord.js
+++ src/lib/server/discord.js
@@ -1,19 +1,11 @@
 import { DISCORD_TOKEN } from '$env/static/private';
 import config from '$lib/config';
 
-/** @type {any} */
-let cache;
-
 export default async function () {
-	if (cache) return cache;
-
 	const response = await fetch(`https://discord.com/api/users/${config.DISCORD}`, {
 		method: 'GET',
 		headers: { Authorization: `Bot ${DISCORD_TOKEN}` }
 	});
 
-	const data = await response.json();
-	cache = data;
-
-	return data;
+	return await response.json();
 }
