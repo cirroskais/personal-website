d63b7c766695b3af5bfe4ad0cd59c0c454207c76
cirroskais <cirroskais@madhouselabs.net>
2024-07-24 04:16:38 -0400

    discord cache

--- src/lib/server/discord.js
+++ src/lib/server/discord.js
@@ -1,11 +1,23 @@
 import { DISCORD_TOKEN } from '$env/static/private';
 import config from '$lib/config';
 
+let cache = {};
+setInterval(
+	() => {
+		cache = {};
+	},
+	1000 * 60 * 60 * 1
+);
+
 export default async function () {
+	if (Object.keys(cache).length) return cache;
+
 	const response = await fetch(`https://discord.com/api/users/${config.DISCORD}`, {
 		method: 'GET',
 		headers: { Authorization: `Bot ${DISCORD_TOKEN}` }
 	});
 
-	return await response.json();
+	const data = await response.json();
+	cache = data;
+	return data;
 }
